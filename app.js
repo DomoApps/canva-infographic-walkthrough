aeb5b9b4fa27c3ccc0c4dc2ff0ca41dfc7ab7820
julian21olarte <julian21olarte@gmail.com>
2024-04-18 11:01:31 -0500

    Add Javascript code and make dynamic data, replace HTML variables

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-// Some DataSets are massive and will bring any web browser to its knees if you
-// try to load the entire thing. To keep your app performing optimally, take
-// advantage of filtering, aggregations, and group by's to bring down just the
-// data your app needs. Do not include all columns in your data mapping file,
-// just the ones you need.
-//
-// For additional documentation on how you can query your data, please refer to
-// https://developer.domo.com/docs/dev-studio/dev-studio-data
-domo.get('/data/v2/infographicData?limit=100')
-    .then(function(infographicData){
-      console.log("infographicData", infographicData);
-    });
+// map structure to link DOM item to dataset column
+const mapDomColumns = [
+  { id: 'title', key: 'city', column: 'city' },
+  { id: 'title', key: 'zip', column: 'zip' },
+  { id: 'homes-listed', key: 'num_homes_listed', column: 'num_homes_listed' },
+  { id: 'avg-dom', key: 'avg_dom', column: 'avg_dom' },
+  { id: 'median-list-price', key: 'median_list_price', column: 'median_list_price', price: true },
+  { id: 'price-change', key: 'price_change_pct', column: 'price_change_pct' },
+  { id: 'market-type', key: 'market_type', column: 'market_type' },
+];
 
+// function to fetch the dataset
+const fetchData = async () => domo.get('/data/v2/infographicData?limit=100&useBeastMode=true');
+
+// formatting price number
+const formatPrice = (price) => {
+  return isNaN(Number(price)) ? price : new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(Number(price));
+}
+
+// function to interpolate the data into the HTML template
+const interpolateData = (data) => {
+  const row = data[0];
+  mapDomColumns.forEach(value => {
+    const defaultValue = document.getElementById(value.id).innerHTML.toString();
+    const columnValue = value.price ? formatPrice(row[value.column]) : row[value.column];
+    const newValue = defaultValue.replaceAll(`{${value.key}}`, columnValue);
+    document.getElementById(value.id).innerHTML = newValue;
+  });
+}
+
+const init = async () => {
+  const data = await fetchData();
+  console.log("data: ", data);
+  interpolateData(data);
+}
