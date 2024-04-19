4dbc47b9d5db332d8b4b7ad6d7d74c99165fae6b
Noah Finberg <noah.finberg@Noah-Finberg-QKMTXMKXG5.local>
2024-04-19 13:52:54 -0600

    fix: update logic ffor formatting marketType.

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const mapDomColumns = [
   { id: 'avg-dom', key: 'avg_dom', column: 'avg_dom' },
   { id: 'median-list-price', key: 'median_list_price', column: 'median_list_price', price: true },
   { id: 'price-change', key: 'price_change_pct', column: 'price_change_pct' },
-  { id: 'market-type', key: 'market_type', column: 'market_type' },
+  { id: 'market-type', key: 'market_type', column: 'market_type', market_type: true },
 ];
 
 // function to fetch the dataset
-const fetchData = async () => domo.get('/data/v2/infographicData?limit=100&useBeastMode=true');
+const fetchData = async () => domo.get('/data/v2/infographicData?limit=100');
 
 // formatting price number
 const formatPrice = (price) => {
@@ -20,13 +20,38 @@ const formatPrice = (price) => {
   }).format(Number(price));
 }
 
+const formatMarketType = (marketType) => {
+  console.log("formatting market type");
+  if (Number(marketType) >= 30) {
+    return `${marketType} (Seller's Market)`
+  } else {
+    return `${marketType} (Buyers's Market)`
+  }
+} 
+
 // function to interpolate the data into the HTML template
 const interpolateData = (data) => {
   const row = data[0];
+  console.log("interpolating data");
+  console.log(row);
+
   mapDomColumns.forEach(value => {
+    let columnValue = "";
     const defaultValue = document.getElementById(value.id).innerHTML.toString();
-    const columnValue = value.price ? formatPrice(row[value.column]) : row[value.column];
+    console.log("value", value);
+
+    if (value.price) {
+      columnValue = formatPrice(row[value.column]);
+    } else if (value.market_type) {
+      columnValue = formatMarketType(row[value.column]);
+    } else {
+      columnValue = row[value.column];
+    }
+
+    console.log(columnValue);
     const newValue = defaultValue.replaceAll(`{${value.key}}`, columnValue);
+    console.log(newValue);
+
     document.getElementById(value.id).innerHTML = newValue;
   });
 }
